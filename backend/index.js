3448e741e944a0731312338131106002aac36156
Phu Vo <u1153426@umail.usq.edu.au>
2024-10-18 23:42:26 +1000

    Project Setup + MongoDB setup + create Models

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+
+dotenv.config({});
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running at port ${PORT}`);
+});
