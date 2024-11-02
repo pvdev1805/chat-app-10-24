e545769b1e58b9ae6f2814556eda0e19cc872ef6
Phu Vo <u1153426@umail.usq.edu.au>
2024-11-03 00:36:03 +1000

    Handle onSubmit event in Login page

--- frontend/src/pages/LogIn/index.jsx
+++ frontend/src/pages/LogIn/index.jsx
@@ -1,13 +1,53 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const LogIn = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`${BASE_URL}/api/v1/user/login`, user, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+
+      navigate("/");
+
+      console.log(res);
+    } catch (error) {
+      console.log("Error! Something went wrong with onSubmitHandler in Login!");
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
+
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
+
   return (
     <>
       <div className="min-w-96 mx-auto">
         <div className="p-6 w-full rounded-lg shadow-md bg-gray-400 glassmorphism-effect">
           <h2 className="text-3xl font-bold text-center">Login</h2>
 
-          <form>
+          <form onSubmit={onSubmitHandler} action="">
             <div>
               <label className="label p-2">
                 <span className="text-base label-text">Username:</span>
@@ -16,6 +56,8 @@ const LogIn = () => {
                 className="w-full input input-bordered h-10"
                 type="text"
                 placeholder="Username"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
               />
             </div>
 
@@ -27,6 +69,8 @@ const LogIn = () => {
                 className="w-full input input-bordered h-10"
                 type="password"
                 placeholder="Password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
               />
             </div>
 
