b569e93c13a5d0b7b42e99405685c25dd312df0a
Phu Vo <u1153426@umail.usq.edu.au>
2024-11-03 19:32:44 +1000

    Configure CORS to allow requests from frontend only

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userRoutes from "./routes/user.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({});
 
@@ -12,7 +14,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOption));
 
 // routes
 app.use("/api/v1/user", userRoutes);
